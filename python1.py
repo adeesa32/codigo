09292db3715e4d1dda0f5aa68e49b13ff62996bc
Adelina Escobar Acevedo <57870687+adeesa32@users.noreply.github.com>
2022-02-21 15:10:31 -0600

    Archivo Python
    Primeros pininos con las katas en python

---
+++ python1.py
@@ -0,0 +1,21 @@
+
+# Importamos la biblioteca 
+from datetime import date
+
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+print(type(distancia_a_alfa_centauri))
+
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+print("Today's date is: " + str(date.today()))
